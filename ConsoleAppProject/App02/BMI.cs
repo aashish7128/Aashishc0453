5fd36b564f5510877b79dcc5178887717686df77
Aashish B K <114670572+aashish7128@users.noreply.github.com>
2023-03-18 11:46:50 +0000

    Update BMI.cs

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -4,7 +4,7 @@
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Aashish Version 0.1
     /// </author>
     public class BMI
     {
