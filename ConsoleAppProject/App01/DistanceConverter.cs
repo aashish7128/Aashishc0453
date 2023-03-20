f8db4af40801a3fc08aa6db67fca9547b1a01d58
aashish7128 <bkaashish644@gmail.com>
2023-03-20 08:48:48 +0000

    Changes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,12 +3,110 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// It is a distance converter which converts the distance in different forms.
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Aashish version 0.1
     /// </author>
     public class DistanceConverter
-    {
-    }
+    { 
+ const int Miles_To_Feet = 5280;
+ const int Feet_To_Miles = 5280;
+ const double Miles_To_Metres = 1609.34;
+ const double Metres_In_Miles = 0.000621371;
+ const double Feets_To_Meters = 0.3048;
+ public double Miles; public double Feets;
+ public double Metres;
+ public string From_distance;
+ public string To_distance;
+
+ public void run()
+ {
+Console.WriteLine("Enter from distance: ");
+From_distance = UnitMenu();
+Console.WriteLine("Enter to distance: ");
+To_distance = UnitMenu();
+if (From_distance == "Miles")
+{
+Miles = input("miles");
+
+if (To_distance == "Feets")
+{
+Feets = Miles * Miles_To_Feet;
+Console.WriteLine (Miles + " miles is equal to " + Feets + " feets.");
 }
+else if (To_distance == "Metres")
+{
+Metres = Miles * Miles_To_Metres;
+Console.WriteLine(Miles + " miles is equal to " + Metres + " metres.");
+}
+}
+else if (From_distance == "Feets")
+{
+    Feets = input("feets");
+    if (To_distance == "Miles")
+    {
+        Miles = Feets / Feet_To_Miles;
+Console. WriteLine(Feets + " Feets is equal to " + Miles + " Miles.");
+    }
+else if (To_distance == "Meters")
+{
+Metres = Feets * Feets_To_Meters;
+Console.WriteLine(Feets + " Feets is equal to " + Metres + " metres.");
+}
+}
+else if (From_distance == "Meters")
+{
+Metres = input("metres");
+
+if (To_distance == "Miles")
+{
+Miles = Metres * Metres_In_Miles;
+Console.WriteLine(Metres + " Metres is equal to " + Miles + " Miles.");
+}
+else if (To_distance == "Feets")
+{
+Feets = Metres / Feets_To_Meters;
+Console.WriteLine (Metres + " Metres is equal to " + Feets +" Feets.");
+}
+}
+ }
+
+static string UnitMenu()
+{
+    Console.WriteLine("Choose the unit for conversion process!");
+    Console.WriteLine("1. Miles");
+    Console.WriteLine("2. Feets");
+    Console.WriteLine("3. Metres");
+    
+    string choice = Console.ReadLine();
+    if (choice != "1" && choice != "2" && choice != "3")
+    {
+throw new Exception("Invalid Choice");
+    }
+Console.WriteLine("Enter the number to convert a unit from one distance to another:");
+if (choice == "1")
+{
+return "Miles";
+}
+else if (choice == "2")
+{
+return "Feets";
+}
+else if (choice == "3")
+{
+return "Meters";
+}
+return null;
+}
+
+Double input(string prompt)
+{
+    Console.WriteLine("Please, enter the number of  " + prompt);
+    return Convert.ToDouble(Console.ReadLine());
+}
+    }
+ 
+
+
+ 
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleAppProject
     /// to start App01 to App05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 05/02/2022
+    ///  05/02/2022
     /// </summary>
     public static class Program
     {
@@ -22,12 +22,31 @@ namespace ConsoleAppProject
             Console.WriteLine();
             Console.WriteLine(" =================================================");
             Console.WriteLine("    BNU CO453 Applications Programming 2021-2022! ");
-            Console.WriteLine("        by Derek                                  ");
+            Console.WriteLine("        by Aashish                                  ");
             Console.WriteLine(" =================================================");
-            Console.WriteLine();
+            Console.WriteLine("Which App would you like to run?");
+            string App = Console.ReadLine();
+            Console.WriteLine("You can work in the selected applications");
+            if (App == "1")
+            {
+                DistanceConverter converter = new DistanceConverter();
+                converter.run();
+            }
+            else if (App == "2")
+            {
+                BMI conversion = new BMI();
+                conversion.run();
+            }
+            else if (App == "3")
+            {
+                StudentsGrades converter = new StudentGrades();
+                converter.run();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Choice");
+            }
 
-            DistanceConverter converter = new DistanceConverter();
-            converter.Run();
         }
     }
 }
